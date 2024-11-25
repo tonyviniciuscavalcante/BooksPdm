3a8ada4162f7c22b782b2f64450f4956f89ba29b
Tony Vinicius Cavalcante <82682652+tonyviniciuscavalcante@users.noreply.github.com>
2024-11-25 20:47:32 -0300

    Update BookAdapter.kt

--- app/src/main/java/com/example/bookspdm/ui/BookAdapter.kt
+++ app/src/main/java/com/example/bookspdm/ui/BookAdapter.kt
@@ -1,16 +1,36 @@
 package com.example.bookspdm.ui
 
 import android.content.Context
+import android.content.Context.LAYOUT_INFLATER_SERVICE
+import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ArrayAdapter
+import android.widget.TextView
 import com.example.bookspdm.R
 import com.example.bookspdm.model.Book
 
 class BookAdapter(context: Context, private val bookList: MutableList<Book>): ArrayAdapter<Book>(context, R.layout.tile_book, bookList) {
 
     override fun getView(position: Int, convertView: View?, parent: ViewGroup): View {
-        return super.getView(position, convertView, parent)
+        // Pegar o livro que vai ser usado para preencher a célula
+        val book = bookList[position]
+
+        // Verificando se a célula já foi inclada ou nāo
+        var bookTile = convertView
+        if (bookTile == null) {
+            // Se nāo foi, infla uma nova célula
+            bookTile = (context.getSystemService(LAYOUT_INFLATER_SERVICE) as LayoutInflater).inflate(R.layout.tile_book, parent, false)
+        }
+
+        // Preenche os valores das célula com o novo livro
+        bookTile?.findViewById<TextView>(R.id.titleTv)?.text = book.title
+        bookTile?.findViewById<TextView>(R.id.titleTv)?.text = book.firstAuthor
+        bookTile?.findViewById<TextView>(R.id.titleTv)?.text = book.edition.toString()
+
+
+        // Retorna a célula preenchida
+        return bookTile!!
     }
 
 }
