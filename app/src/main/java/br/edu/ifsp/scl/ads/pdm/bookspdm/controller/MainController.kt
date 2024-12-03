266e99a8be0382d1812275934ece56d12fce100f
Tony Vinicius Cavalcante <82682652+tonyviniciuscavalcante@users.noreply.github.com>
2024-12-02 21:46:56 -0300

    Update MainController.kt

--- app/src/main/java/br/edu/ifsp/scl/ads/pdm/bookspdm/controller/MainController.kt
+++ app/src/main/java/br/edu/ifsp/scl/ads/pdm/bookspdm/controller/MainController.kt
@@ -1,4 +1,16 @@
 package br.edu.ifsp.scl.ads.pdm.bookspdm.controller
 
-class MainController {
+import br.edu.ifsp.scl.ads.pdm.bookspdm.model.Book
+import br.edu.ifsp.scl.ads.pdm.bookspdm.model.BookDao
+import br.edu.ifsp.scl.ads.pdm.bookspdm.model.BookSqliteImpl
+import br.edu.ifsp.scl.ads.pdm.bookspdm.ui.MainActivity
+
+class MainController(mainActivity: MainActivity) {
+    private val bookDao: BookDao = BookSqliteImpl(mainActivity)
+
+    fun insertBook(book: Book) = bookDao.createBook(book)
+    fun getBook(isbn: String) = bookDao.retriveBook(isbn)
+    fun getBooks() = bookDao.retriveBooks()
+    fun modifyBook(book: Book) = bookDao.updateBook(book)
+    fun removeBook(isbn: String) = bookDao.deleteBook(isbn)
 }
